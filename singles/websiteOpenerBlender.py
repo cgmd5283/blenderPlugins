78045ab50e883578592fdac43caafd8e9963c626
cgmd5283 <58121944+cgmd5283@users.noreply.github.com>
2023-03-28 00:36:18 -0400

    made installable as an add on

--- singles/websiteOpenerBlender.py
+++ singles/websiteOpenerBlender.py
@@ -1,28 +1,39 @@
 import bpy
 import webbrowser
+bl_info = {
+    "name": "Website Opener",
+    "version": (1, 0),
+    "blender": (2, 80, 0),
+    "category": "Object",
+    "author": "Your Name",
+    "description": "Open websites with confirmation dialog"
+}
+
 
 class OpenWebsiteOperator(bpy.types.Operator):
     bl_idname = "object.open_website"
     bl_label = "open website"
-    
+
     def execute(self, context):
         confirmed = bpy.ops.wm.modal_confirm('INVOKE_AREA')
         if not confirmed:
             return {'CANCELLED'}
         return {'FINISHED'}
-        
-    
+
+
 class ModalConfirmOperator(bpy.types.Operator):
     bl_idname = "wm.modal_confirm"
     bl_label = "Confirm"
-    
+
     def execute(self, context):
         webbrowser.open("https://handf.us")
         return {'FINISHED'}
-    
+
     def invoke(self, context, event):
         wm = context.window_manager
         return wm.invoke_confirm(self, event)
+
+
 class ModalConfirmOperator1(bpy.types.Operator):
     bl_idname = "wm.modal_confirm1"
     bl_label = "Confirm 1"
@@ -48,16 +59,17 @@ class ModalConfirmOperator2(bpy.types.Operator):
         wm = context.window_manager
         return wm.invoke_confirm(self, event)
 
+
 class OpenWebsitePanel(bpy.types.Panel):
     bl_idname = "VIEW3D_PT_open_website_panel"
     bl_label = "Open Website Panel"
     bl_space_type = 'VIEW_3D'
     bl_region_type = 'UI'
     bl_category = 'Tool'
-    
+
     def draw(self, context):
         layout = self.layout
-        
+
         row = layout.row()
         row.operator(OpenWebsiteOperator.bl_idname, text="Open Website")
         row = layout.row()
@@ -65,19 +77,22 @@ class OpenWebsitePanel(bpy.types.Panel):
         row = layout.row()
         row.operator(ModalConfirmOperator2.bl_idname, text="Example 2")
 
+
 def register():
     bpy.utils.register_class(OpenWebsiteOperator)
     bpy.utils.register_class(OpenWebsitePanel)
     bpy.utils.register_class(ModalConfirmOperator)
-    bpy.utils.register_class(ModalConfirmOperator1) 
-    bpy.utils.register_class(ModalConfirmOperator2) 
+    bpy.utils.register_class(ModalConfirmOperator1)
+    bpy.utils.register_class(ModalConfirmOperator2)
+
 
 def unregister():
     bpy.utils.unregister_class(OpenWebsiteOperator)
     bpy.utils.unregister_class(OpenWebsitePanel)
     bpy.utils.unregister_class(ModalConfirmOperator)
-    bpy.utils.register_class(ModalConfirmOperator1) 
-    bpy.utils.register_class(ModalConfirmOperator2)  
+    bpy.utils.unregister_class(ModalConfirmOperator1)
+    bpy.utils.unregister_class(ModalConfirmOperator2)
+
 
 if __name__ == "__main__":
     register()
